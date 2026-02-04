5e47b1f65c0d0f214ece15f73b28be5e70e0fcac
Md. Monjurul Haque <himel452@gmail.com>
2026-02-04 23:32:58 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'khona-v1';
+const ASSETS_TO_CACHE = [
+    './index.html',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;600;700&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// 1. Install Event: Cache Assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// 2. Fetch Event: Serve from Cache if offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// 3. Activate Event: Clean old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
